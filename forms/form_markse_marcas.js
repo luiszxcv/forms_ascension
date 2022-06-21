cf65c267ceb2e4c5db446f54e9efeed34de407a8
luiszxcv <luisfelipesantoszxcv@hotmail.com>
2022-06-21 01:22:31 -0400

    css corrections

--- forms/form_markse_marcas.js
+++ forms/form_markse_marcas.js
@@ -1,137 +1,14 @@
+var _0x45da5d = _0x236f;
+
+function _0x4a46() { var _0xb6f9b1 = ['headline', 'modal-ascension', '\x0a\x20\x20\x20\x20<div>\x0a\x20\x20\x20\x20\x20\x20<div\x20class=\x22section_form_ascension\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<input\x20class=\x22modal_form_ascension-btn\x22\x20type=\x22checkbox\x22\x20id=\x22modal_form_ascension-btn\x22\x20name=\x22modal_form_ascension-btn\x22/>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<label\x20for=\x22modal_form_ascension-btn\x22\x20id=\x22label-whatsapp-fixed-icon\x22></label>\x09\x0a\x20\x20\x20\x20\x20\x20\x09<div\x20class=\x22modal_form_ascension\x22>\x09\x09\x0a\x09\x20\x20\x20\x20\x20\x20\x09<div\x20class=\x22modal_form_ascension-wrap\x20padding-horizontal--48\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20', '2548115XwPliM', '\x0a\x20\x20\x20\x20<div>\x0a\x20\x20\x20\x20\x20\x20<div\x20class=\x22section_form_ascension\x22>\x0a\x20\x20\x20\x20\x20\x20\x09<input\x20style=\x22\x22class=\x22modal_form_ascension-btn\x22\x20type=\x22checkbox\x22\x20id=\x22modal_form_ascension-btn\x22\x20name=\x22modal_form_ascension-btn\x22/>\x0a\x20\x20\x20\x20\x20\x20\x09<label\x20for=\x22modal_form_ascension-btn\x22\x20id=\x22label-whatsapp-fixed-icon\x22><svg\x20viewBox=\x220\x200\x2032\x2032\x22\x20class=\x22whatsapp-ico_fixed_ascension\x22><path\x20d=\x22\x20M19.11\x2017.205c-.372\x200-1.088\x201.39-1.518\x201.39a.63.63\x200\x200\x201-.315-.1c-.802-.402-1.504-.817-2.163-1.447-.545-.516-1.146-1.29-1.46-1.963a.426.426\x200\x200\x201-.073-.215c0-.33.99-.945.99-1.49\x200-.143-.73-2.09-.832-2.335-.143-.372-.214-.487-.6-.487-.187\x200-.36-.043-.53-.043-.302\x200-.53.115-.746.315-.688.645-1.032\x201.318-1.06\x202.264v.114c-.015.99.472\x201.977\x201.017\x202.78\x201.23\x201.82\x202.506\x203.41\x204.554\x204.34.616.287\x202.035.888\x202.722.888.817\x200\x202.15-.515\x202.478-1.318.13-.33.244-.73.244-1.088\x200-.058\x200-.144-.03-.215-.1-.172-2.434-1.39-2.678-1.39zm-2.908\x207.593c-1.747\x200-3.48-.53-4.942-1.49L7.793\x2024.41l1.132-3.337a8.955\x208.955\x200\x200\x201-1.72-5.272c0-4.955\x204.04-8.995\x208.997-8.995S25.2\x2010.845\x2025.2\x2015.8c0\x204.958-4.04\x208.998-8.998\x208.998zm0-19.798c-5.96\x200-10.8\x204.842-10.8\x2010.8\x200\x201.964.53\x203.898\x201.546\x205.574L5\x2027.176l5.974-1.92a10.807\x2010.807\x200\x200\x200\x2016.03-9.455c0-5.958-4.842-10.8-10.802-10.8z\x22\x20fill-rule=\x22evenodd\x22></path></svg></label>\x09\x0a\x09\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20</div>\x0a\x20\x20', '554931RKRNoZ', '873288QqLFya', '894Srjfmj', 'VueSelect', '296627UKmORt', 'modal-fixed-modal-ascension', 'telefone', '1350184XlQMwl', 'button_text', 'https://forms.ascensiondigital.com.br/src/forms/markse_marcas.html', 'component', '6456192AfPANX', 'modal-form-ascension', 'redirect_url', 'formid', '\x0a\x20\x20\x20\x20<div>\x0a\x20\x20\x20\x20\x20\x20<div\x20class=\x22section_form_ascension\x22>\x0a\x20\x20\x20\x20\x20\x20\x09<input\x20class=\x22modal_form_ascension-btn\x22\x20type=\x22checkbox\x22\x20id=\x22modal_form_ascension-btn\x22\x20name=\x22modal_form_ascension-btn\x22/>\x0a\x20\x20\x20\x20\x20\x20\x09<label\x20for=\x22modal_form_ascension-btn\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20{{button_text}}\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20&nbsp;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<svg\x20xmlns=\x22http://www.w3.org/2000/svg\x22\x20width=\x2216\x22\x20height=\x2216\x22\x20fill=\x22currentColor\x22\x20class=\x22bi\x20bi-arrow-right-circle-fill\x22\x20viewBox=\x220\x200\x2016\x2016\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<path\x20d=\x22M8\x200a8\x208\x200\x201\x201\x200\x2016A8\x208\x200\x200\x201\x208\x200zM4.5\x207.5a.5.5\x200\x200\x200\x200\x201h5.793l-2.147\x202.146a.5.5\x200\x200\x200\x20.708.708l3-3a.5.5\x200\x200\x200\x200-.708l-3-3a.5.5\x200\x201\x200-.708.708L10.293\x207.5H4.5z\x22/>\x0a\x20\x20\x20\x20\x20\x20\x20\x20</svg></label>\x20\x09\x09\x0a\x09\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20', '#form_markse_marcas', '17913FXSjhy', 'v-select'];
+    _0x4a46 = function() { return _0xb6f9b1; }; return _0x4a46(); }(function(_0x177135, _0x1c90c6) { var _0x33cde5 = _0x236f,
+        _0x260a11 = _0x177135(); while (!![]) { try { var _0x3803f7 = -parseInt(_0x33cde5(0x1e8)) / 0x1 + -parseInt(_0x33cde5(0x1e5)) / 0x2 + -parseInt(_0x33cde5(0x1e4)) / 0x3 + parseInt(_0x33cde5(0x1eb)) / 0x4 + parseInt(_0x33cde5(0x1e2)) / 0x5 + parseInt(_0x33cde5(0x1e6)) / 0x6 * (-parseInt(_0x33cde5(0x1f5)) / 0x7) + parseInt(_0x33cde5(0x1ef)) / 0x8; if (_0x3803f7 === _0x1c90c6) break;
+            else _0x260a11['push'](_0x260a11['shift']()); } catch (_0x1d9c46) { _0x260a11['push'](_0x260a11['shift']()); } } }(_0x4a46, 0x5695e));
 import { checkForm, send, telefone, validForm, loadHTML } from '/src/js/functions_globals.js';
+
+function _0x236f(_0x36aec7, _0x4af30e) { var _0x4a460e = _0x4a46(); return _0x236f = function(_0x236f11, _0x5410c6) { _0x236f11 = _0x236f11 - 0x1e1; var _0x4e439e = _0x4a460e[_0x236f11]; return _0x4e439e; }, _0x236f(_0x36aec7, _0x4af30e); }
 import { options_markse } from '/src/js/variables.js';
-var form = "";
-await loadHTML("https://forms.ascensiondigital.com.br/src/forms/markse_marcas.html").then(res => {
-    form = res;
-});
-
-Vue.component('v-select', VueSelect.VueSelect);
-
-Vue.component('modal-ascension', {
-    props: ['button_text', "redirect_url", "formid", "headline"],
-    data: function() {
-        return {
-            form: form,
-            options: options_markse,
-            customFields: {},
-            errors: [],
-            validForm: validForm,
-            send: send,
-            telefone: telefone,
-            checkForm: checkForm,
-        }
-    },
-    methods: {},
-    mounted() {
-
-    },
-    watch: {
-        customFields: {
-            handler(val) {
-                if (val.telefone) {
-                    val.telefone = this.telefone(val.telefone);
-                }
-            },
-            deep: true
-        }
-    },
-    template: `
-    <div>
-      <div class="section_form_ascension">
-        <input class="modal_form_ascension-btn" type="checkbox" id="modal_form_ascension-btn" name="modal_form_ascension-btn"/>
-        <label for="modal_form_ascension-btn" id="label-whatsapp-fixed-icon"></label>	
-      	<div class="modal_form_ascension">		
-	      	<div class="modal_form_ascension-wrap padding-horizontal--48">
-          ` + form + `
-	      	</div>			          		
-      	</div>	
-          <img class="scrool_ascension"  src="https://forms.ascensiondigital.com.br/src/img/scroll-icon-png-22.jpg" alt="scrool">
-	    </div>
-    </div>
-  `
-})
-
-Vue.component('form-ascension', {
-    props: ['button_text', "redirect_url", "formid", "headline"],
-    data: function() {
-        return {
-            form: form,
-            options: options_markse,
-            customFields: {},
-            errors: [],
-            validForm: validForm,
-            send: send,
-            telefone: telefone,
-            checkForm: checkForm,
-        }
-    },
-    methods: {},
-    mounted() {
-
-    },
-    watch: {
-        customFields: {
-            handler(val) {
-                if (val.telefone) {
-                    val.telefone = this.telefone(val.telefone);
-                }
-            },
-            deep: true
-        }
-    },
-    template: `
-        <div>
-            <div id="html-form-ascension" >
-            ` + form + `
-            </div>
-        </div>`
-})
-
-Vue.component('modal-form-ascension', {
-    props: ['button_text'],
-    data: function() {
-        return {}
-    },
-    mounted() {},
-    watch: {},
-    template: `
-    <div>
-      <div class="section_form_ascension">
-      	<input class="modal_form_ascension-btn" type="checkbox" id="modal_form_ascension-btn" name="modal_form_ascension-btn"/>
-      	<label for="modal_form_ascension-btn">
-          {{button_text}}
-          &nbsp;
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-          <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-        </svg></label> 		
-	    </div>
-    </div>
-    `
-})
-
-
-Vue.component('modal-fixed-modal-ascension', {
-    data: function() {
-        return {}
-    },
-    methods: {},
-    mounted() {
-
-    },
-    watch: {},
-    template: `
-    <div>
-      <div class="section_form_ascension">
-      	<input style=""class="modal_form_ascension-btn" type="checkbox" id="modal_form_ascension-btn" name="modal_form_ascension-btn"/>
-      	<label for="modal_form_ascension-btn" id="label-whatsapp-fixed-icon"><svg viewBox="0 0 32 32" class="whatsapp-ico_fixed_ascension"><path d=" M19.11 17.205c-.372 0-1.088 1.39-1.518 1.39a.63.63 0 0 1-.315-.1c-.802-.402-1.504-.817-2.163-1.447-.545-.516-1.146-1.29-1.46-1.963a.426.426 0 0 1-.073-.215c0-.33.99-.945.99-1.49 0-.143-.73-2.09-.832-2.335-.143-.372-.214-.487-.6-.487-.187 0-.36-.043-.53-.043-.302 0-.53.115-.746.315-.688.645-1.032 1.318-1.06 2.264v.114c-.015.99.472 1.977 1.017 2.78 1.23 1.82 2.506 3.41 4.554 4.34.616.287 2.035.888 2.722.888.817 0 2.15-.515 2.478-1.318.13-.33.244-.73.244-1.088 0-.058 0-.144-.03-.215-.1-.172-2.434-1.39-2.678-1.39zm-2.908 7.593c-1.747 0-3.48-.53-4.942-1.49L7.793 24.41l1.132-3.337a8.955 8.955 0 0 1-1.72-5.272c0-4.955 4.04-8.995 8.997-8.995S25.2 10.845 25.2 15.8c0 4.958-4.04 8.998-8.998 8.998zm0-19.798c-5.96 0-10.8 4.842-10.8 10.8 0 1.964.53 3.898 1.546 5.574L5 27.176l5.974-1.92a10.807 10.807 0 0 0 16.03-9.455c0-5.958-4.842-10.8-10.802-10.8z" fill-rule="evenodd"></path></svg></label>	
-	    </div>
-    </div>
-  `
-});
-
-
-
-new Vue({
-    el: '#form_markse_marcas',
-    data: {}
-})
+var form = '';
+await loadHTML(_0x45da5d(0x1ed))['then'](_0x4442b6 => { form = _0x4442b6; }), Vue[_0x45da5d(0x1ee)](_0x45da5d(0x1f6), VueSelect[_0x45da5d(0x1e7)]), Vue[_0x45da5d(0x1ee)](_0x45da5d(0x1f8), { 'props': ['button_text', _0x45da5d(0x1f1), _0x45da5d(0x1f2), _0x45da5d(0x1f7)], 'data': function() { return { 'form': form, 'options': options_markse, 'customFields': {}, 'errors': [], 'validForm': validForm, 'send': send, 'telefone': telefone, 'checkForm': checkForm }; }, 'methods': {}, 'mounted' () {}, 'watch': { 'customFields': { 'handler' (_0xd52598) { var _0x49ccb6 = _0x45da5d;
+                _0xd52598[_0x49ccb6(0x1ea)] && (_0xd52598[_0x49ccb6(0x1ea)] = this[_0x49ccb6(0x1ea)](_0xd52598[_0x49ccb6(0x1ea)])); }, 'deep': !![] } }, 'template': _0x45da5d(0x1e1) + form + '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<img\x20class=\x22scrool_ascension\x22\x20\x20src=\x22https://forms.ascensiondigital.com.br/src/img/scroll-icon-png-22.jpg\x22\x20alt=\x22scrool\x22>\x0a\x09\x20\x20\x20\x20\x20\x20\x09</div>\x09\x09\x09\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x09\x09\x0a\x20\x20\x20\x20\x20\x20\x09</div>\x09\x0a\x09\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20</div>\x0a\x20\x20' }), Vue[_0x45da5d(0x1ee)]('form-ascension', { 'props': [_0x45da5d(0x1ec), 'redirect_url', _0x45da5d(0x1f2), 'headline'], 'data': function() { return { 'form': form, 'options': options_markse, 'customFields': {}, 'errors': [], 'validForm': validForm, 'send': send, 'telefone': telefone, 'checkForm': checkForm }; }, 'methods': {}, 'mounted' () {}, 'watch': { 'customFields': { 'handler' (_0x2fdf98) { var _0x55329c = _0x45da5d;
+                _0x2fdf98[_0x55329c(0x1ea)] && (_0x2fdf98[_0x55329c(0x1ea)] = this[_0x55329c(0x1ea)](_0x2fdf98[_0x55329c(0x1ea)])); }, 'deep': !![] } }, 'template': '\x0a\x20\x20\x20\x20\x20\x20\x20\x20<div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20id=\x22html-form-ascension\x22\x20>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20' + form + '\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20</div>' }), Vue[_0x45da5d(0x1ee)](_0x45da5d(0x1f0), { 'props': [_0x45da5d(0x1ec)], 'data': function() { return {}; }, 'mounted' () {}, 'watch': {}, 'template': _0x45da5d(0x1f3) }), Vue['component'](_0x45da5d(0x1e9), { 'data': function() { return {}; }, 'methods': {}, 'mounted' () {}, 'watch': {}, 'template': _0x45da5d(0x1e3) }), new Vue({ 'el': _0x45da5d(0x1f4), 'data': {} });
